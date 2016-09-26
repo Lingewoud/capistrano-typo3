1021be4ffea647e9c9691cc11e265e270625372c
Pim Snel <pim@lingewoud.nl>
2016-09-26 17:20:40 +0200

    cleanup code

--- lib/capistrano/typo3.rb
+++ lib/capistrano/typo3.rb
@@ -1,7 +1,6 @@
 require "capistrano/typo3/version"
 require "capistrano/typo3/typo3_helper"
 require "capistrano/typo3/dt3_div"
-#require "capistrano/typo3/dt3_logger"
 require "capistrano/typo3/dt3_mysql"
 require 'yaml' # Built in, no gem required
 
