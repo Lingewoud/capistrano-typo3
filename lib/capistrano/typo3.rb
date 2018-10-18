2fd8eb4384d42103f8d61bef99ecd876d9963d56
Pim Snel <pim@lingewoud.nl>
2018-10-18 15:38:08 +0200

    refactor and add wordpress

--- lib/capistrano/typo3.rb
+++ lib/capistrano/typo3.rb
@@ -6,8 +6,6 @@ require "capistrano/typo3/dt3_div"
 require "capistrano/typo3/dt3_mysql"
 require 'yaml' # Built in, no gem required
 
-
-
 TYPO3_DB_DUMP_DIR = 'db_dumps'
 
 load File.expand_path('../tasks/typo3.cap', __FILE__)
@@ -16,9 +14,13 @@ load File.expand_path('../tasks/typo3helper.cap', __FILE__)
 load File.expand_path('../tasks/typo3homestead.cap', __FILE__)
 load File.expand_path('../tasks/typo3test.cap', __FILE__)
 load File.expand_path('../tasks/typo3vagrant.cap', __FILE__)
+
+load File.expand_path('../tasks/wp_homestead.cap', __FILE__)
+
 load File.expand_path('../tasks/deploy.cap', __FILE__)
 load File.expand_path('../tasks/git.cap', __FILE__)
 load File.expand_path('../tasks/db.cap', __FILE__)
+load File.expand_path('../tasks/sync.cap', __FILE__)
 
 
 
